a04f1f3854037091518b97c406c7bb8b69545b82
vladbuhaescu <buhaescuvlad@yahoo.com>
2025-11-19 14:17:37 +0200

    11 Add go router dependency and basic approuter (#19)
    * add go router setup
    
    ---------
    
    Co-authored-by: Vlad Buhaescu <vlad.buhaescu@qoobiss.com>

--- app/gym_navigation/lib/src/router.dart
+++ app/gym_navigation/lib/src/router.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:test_feat/test_feat.dart';
+
 
 final router = GoRouter(
+  initialLocation: '/test-feat',
   routes: [
     GoRoute(
       path: '/',
       builder: (context, state) => SizedBox(),
-    )
+    ),
+    GoRoute(
+      path: '/test-feat',
+      builder: (context, state) => TestFeatFlow(),
+      
+    ),
   ],
   errorBuilder: (context, state) => Scaffold(
     appBar: AppBar(),
--- app/gym_navigation/route_information.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'router.dart';
-
-RouteInformationProvider get routeInformationProvider =>
-    router.routeInformationProvider;
-RouteInformationParser<Object> get routeInformationParser =>
-    router.routeInformationParser;
-RouterDelegate get routerDelegate => router.routerDelegate;
